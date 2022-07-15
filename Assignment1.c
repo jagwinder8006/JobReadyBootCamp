9022457a1e469bec7d6d80b1f3a8d802effef050
jagwinder singh <78211341+jagwinder8006@users.noreply.github.com>
2022-07-15 09:24:50 +0530

    It is my First assignment.

---
+++ Assignment1.c
@@ -0,0 +1,185 @@
+                                                      // Assignment no.1
+//1. Write a program to print Hello Students on the screen.
+
+/*
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("Hello Students");
+    getch();
+    return 0;
+}
+
+
+//2. Write a program to print Hello in the first line and Students in the second line.
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("Hello\nStudents");
+    getch();
+    return 0;
+}
+
+
+//3. Write a program to print “MySirG” on the screen. (Remember to print in double quotes)
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("\"My sirg\"");
+    getch();
+    return 0;
+
+}
+
+
+// 4. WAP to find the area of the circle. Take radius of circle from user as input and print the result in below given format.
+//Expected output format – “Area of circle is A having the radius R”. Replace A with area & R with radius.
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    float R;
+    double A;
+    printf("Enter Radius=");
+    scanf("%f",&R);
+    A=22*R*R/7;
+    printf("Area of circle is %lf having the radius %0.1f",A,R);
+    getch();
+    return 0;
+}
+
+// 5.WAP to calculate the length of String using printf function.
+
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int x=printf("jagwinder singh");
+   printf("%d",x);
+   getch();
+    return 0;
+}
+
+
+
+//6.WAP to print the name of the user in double quotes. Expected output format – “Hello , Amit Kumar”
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    printf("\"Hello\, Amit kumar\"");
+    getch();
+    return 0;
+}
+
+
+
+//7. WAP to print “%d” on the screen.
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf(" %c%c",'%','d');
+    getch();
+    return 0;
+}
+
+//8. WAP to print “\n” on the screen.
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("\"\\n\"");
+    getch();
+    return 0;
+}
+
+//9. WAP to print “\\” on the screen.
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("\"\\\\\"");
+    getch();
+    return 0;
+}
+
+
+//10. WAP to take date as an input in below given format and convert the date format and     display the result as given below.
+//User Input date format – “DD/MM/YYYY”  (27/11/2022)
+//Output format –
+//“Day – DD , Month – MM , Year – YYYY” (Day – 27 ,Month – 11 , Year – 2022)
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int day,month,year;
+  printf("DD/MM/YY=");
+  scanf("%d%d%d",&day,&month,&year);
+  printf("Day - %d , Month - %d , year - %d ",day,month,year);
+  getch();
+  return 0;
+
+}
+
+
+//11. WAP to take time as an input in below given format and convert the time format and display the result as given below.
+//User Input date format – “HH:MM”
+//Output format – “HH hour and MM Minute”
+//Example –
+//“11:25” converted to “11 Hour and 25 Minute”
+
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int h,m;
+    printf("Enter time this format \"HH:MM\"=");
+    scanf("%d%d",&h,&m);
+    printf("HH %d MM %d",h,m);
+    getch();
+    return 0;
+}
+
+
+//12. Find output of below code:
+
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int x=printf("ineuron");
+   printf("%d",x);
+    getch();
+    return 0;
+}
+
+//ITS ANSWER IS "ineuron7"
+*/
+
+
+
+
+
+
+
+
+
+
+
+
