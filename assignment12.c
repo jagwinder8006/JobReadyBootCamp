3562924a99fe2feb8d00e7c644ba68087607d623
jagwinder singh <78211341+jagwinder8006@users.noreply.github.com>
2022-09-17 21:18:27 +0530

    This is Assignment No.12

---
+++ assignment12.c
@@ -0,0 +1,678 @@
+//                                    Assignment - 12 A Job Ready Bootcamp in C++, DSA and IOT  MySirG
+//                                                   Recursion in C Language
+/*
+//1. Write a recursive function to print first N natural numbers.
+#include<stdio.h>
+void Nnatural(int);
+int main()
+{
+    int d;
+    printf("Enter a Number=");
+    scanf("%d",&d);
+    Nnatural(d);
+    return 0;
+}
+
+void Nnatural(int f)
+{
+    if(f==0)
+        return 1;
+    else
+    {
+        Nnatural(f-1);
+        printf("\n%d",f);
+    }
+}
+
+
+//2. Write a recursive function to print first N natural numbers in reverse order
+#include<stdio.h>
+void Nreverse(int);
+int main()
+{
+    int k;
+    printf("Enter a Number=");
+    scanf("%d",&k);
+    Nreverse(k);
+    return 0;
+}
+
+void Nreverse(int d)
+{
+    if(d==0)
+        return 1;
+    else
+    {
+        printf("\n %d",d);
+        Nreverse(d-1);
+    }
+
+
+}
+
+//3. Write a recursive function to print first N odd natural numbers
+#include<stdio.h>
+void Nodd(int);
+int main()
+{
+    int h;
+    printf("Enter a Number=");
+    scanf("%d",&h);
+    Nodd(h);
+    return 0;
+}
+
+void Nodd(int k)
+{
+    if(k==0)
+        return 0;
+    else
+    {
+        Nodd(k-1);
+        printf("\n%d",k*2-1);
+    }
+}
+
+//4. Write a recursive function to print first N odd natural numbers in reverse order.
+#include<stdio.h>
+void Noddreverse(int);
+int main()
+{
+    int g;
+    printf("Enter a Number=");
+    scanf("%d",&g);
+    Noddreverse(g);
+    return 0;
+}
+
+void Noddreverse(int d)
+{
+    if(d==0)
+        return 5;
+    else
+    {
+        printf("\n%d",d*2-1);
+        Noddreverse(d-1);
+    }
+
+}
+
+//5. Write a recursive function to print first N even natural numbers.
+#include<stdio.h>
+void Neven(int);
+int main()
+{
+    int pp;
+    printf("Enter a Number=");
+    scanf("%d",&pp);
+    Neven(pp);
+    return 0;
+}
+
+void Neven(int s)
+{
+    if(s==0)
+        return 0;
+    else
+    {
+        Neven(s-1);
+        printf("\n%d",s*2);
+
+    }
+}
+
+//6. Write a recursive function to print first N even natural numbers in reverse order.
+#include<stdio.h>
+void Nevenreverse(int);
+int main()
+{
+    int j;
+    printf("Enter a Number=");
+    scanf("%d",&j);
+    Nevenreverse(j);
+    return 0;
+}
+
+void Nevenreverse(int k)
+{
+    if(k==0)
+        return 4;
+    else
+    {
+        printf("\n%d",k*2);
+        Nevenreverse(k-1);
+    }
+}
+
+//7. Write a recursive function to print squares of first N natural numbers.
+#include<stdio.h>
+void squares(int);
+int main()
+{
+    int t;
+    printf("Enter a Number=");
+    scanf("%d",&t);
+    squares(t);
+    return 0;
+
+}
+
+void squares(int d)
+{
+    if(d==0)
+        return 1;
+    else
+    {
+        squares(d-1);
+        printf("\n%d",d*d);
+    }
+}
+
+//8. Write a recursive function to print binary of a given decimal number.
+#include<stdio.h>
+void rnumber(int);
+int main()
+{
+    int d;
+    printf("Enter a Number=");
+    scanf("%d",&d);
+    rnumber(d);
+    return 0;
+}
+
+ void rnumber(int h)
+{
+       if(h==0)
+        return 0;
+       else
+      {
+        rnumber(h/2);
+        printf("%d",h%2);
+      }
+}
+
+//9. Write a recursive function to print octal of a given decimal number
+#include<stdio.h>
+void rnumber(int);
+int main()
+{
+    int d;
+    printf("Enter a Number=");
+    scanf("%d",&d);
+    rnumber(d);
+    return 0;
+}
+
+ void rnumber(int h)
+{
+       if(h==0)
+        return 0;
+       else
+      {
+        rnumber(h/8);
+        printf("%d",h%8);
+      }
+}
+
+
+//10. Write a recursive function to print reverse of a given number
+#include<stdio.h>
+void rnumber(int);
+int main()
+{
+    int d;
+    printf("Enter a Number=");
+    scanf("%d",&d);
+    rnumber(d);
+    return 0;
+}
+
+ void rnumber(int h)
+{
+       if(h==0)
+        return 0;
+       else
+      {
+        printf("% d",h%10);
+        rnumber(h/10);
+      }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
